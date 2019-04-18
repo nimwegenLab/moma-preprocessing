7edbce909e401ed63bd0b11c05f957f1a65590e9
Michael Mell <michael.mell@gmx.net>
2019-04-18 15:04:13 +0200

    Add tests for datasets 03 and 04. However they do not yet contain assert-statements.

--- mmpreprocesspy/test/test_preprocessing.py
+++ mmpreprocesspy/test/test_preprocessing.py
@@ -2,41 +2,37 @@ from unittest import TestCase
 
 
 class TestPreprocessing(TestCase):
-    def test_split_channels_init(self):
+    def test_split_channels_init_dataset_03(self):
         from PIL import Image
 
-        #import mmpreprocesspy.preprocessing as pre
-        #import mmpreprocesspy.preprocessing
-        import mmpreprocesspy
-        # from matplotlib.pyplot import imshow
-        # import numpy as np
+        from mmpreprocesspy import preprocessing
+        import numpy as np
 
-        image_path = '/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/first_images/Pos0/img_000000000_ DIA Ph3 (GFP)_000.tif'
+        image_path = '/home/micha/Documents/git/MM_Testing/03_20180604_gluIPTG10uM_lac_lacIoe_1/first_images/Pos0/03_img_000000000_ DIA Ph3 (GFP)_000.tif'
 
         image_base = Image.open(image_path)
+        image_array = np.array(image_base)
+        image_rot, angle, mincol, maxcol, channel_centers = preprocessing.split_channels_init(image_array)
+        print(mincol)
+        print(maxcol)
 
-        # image_base.convert(mode="RGB")
-        image_base.mode = 'I'
-        im2 = image_base.point(lambda i: i * (1. / 256)).convert('L')
-        im2.show()
-        im2.save('/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/first_images/Pos0/img_000000000_ DIA Ph3 (GFP)_000__.jpeg')
+    def test_split_channels_init_dataset_04(self):
+        from PIL import Image
 
+        from mmpreprocesspy import preprocessing
+        import numpy as np
 
+        image_path = '/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/first_images/Pos0/04_img_000000000_ DIA Ph3 (GFP)_000.tif'
 
-        # image_base.save('/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/first_images/Pos0/img_000000000_ DIA Ph3 (GFP)_000__.jpeg')
+        image_base = Image.open(image_path)
+        image_array = np.array(image_base)
+        image_rot, angle, mincol, maxcol, channel_centers = preprocessing.split_channels_init(image_array)
+        print(mincol)
+        print(maxcol)
 
-        # image_base.convert()
-        # table = [i / 256 for i in range(65536)]
-        #
-        # im2 = image_base.point(table, 'L')
-        #
-        # print
-        # im2.mode
-        #
-        # im2.show()
+    # @staticmethod
+    # def show_image(image): # this method is currently broken
+    #     image.mode = 'I'
+    #     im2 = image.point(lambda i: i * (1. / 256)).convert('L')
+    #     im2.show()
 
-        # image_base.convert('L').save('/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/first_images/Pos0/img_000000000_ DIA Ph3 (GFP)_000__.jpeg')
-        # image_base.show()
-        # imshow(np.asarray(image_base))
-        # image_rot, angle, mincol, maxcol, channel_centers = mmpreprocesspy.preprocessing.split_channels_init(image_base)
-        # self.fail()
