6a239147a63c5a42b2817531fdd9afc656c64989
Michael Mell <michael.mell@gmx.net>
2019-04-25 17:37:27 +0200

    Some refactoring. Also prepration for using a transformation matrix for doing image transformations.

--- mmpreprocesspy/mmpreprocesspy/moma_image_processing.py
+++ mmpreprocesspy/mmpreprocesspy/moma_image_processing.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 from mmpreprocesspy.preprocessing import get_growthlane_regions
 from skimage.feature import match_template
-
+import cv2 as cv
 
 class MomaImageProcessor(object):
     """ MomaImageProcessor encapsulates the processing of a Mothermachine image. """
@@ -21,6 +21,8 @@ class MomaImageProcessor(object):
         self.hor_mid = None
         self.hor_width = None
         self.mid_row = None
+        self.vertical_shift = None
+        self.horizontal_shift = None
 
     def load_numpy_image_array(self, image):
         self.image = image
@@ -45,10 +47,30 @@ class MomaImageProcessor(object):
         result = match_template(self.template, image_number_region, pad_input=True)
         ij = np.unravel_index(np.argmax(result), result.shape)
         t1, t0 = ij[::-1]
-        self.t0 = int(t0 - self.template.shape[0] / 2)
-        self.t1 = int(t1 - self.template.shape[1] / 2)
+        self.vertical_shift = int(t0 - self.template.shape[0] / 2)
+        self.horizontal_shift = int(t1 - self.template.shape[1] / 2)
 
     def get_registered_image(self, image_to_register):
-        registered_image = np.roll(image_to_register, (self.t0, self.t1), axis=(0, 1))  # shift image
-        registered_image = skimage.transform.rotate(registered_image, self.main_channel_angle, cval=0)  # rotate image
+        registered_image = self._translate_image(image_to_register)
+        registered_image = self._rotate_image(registered_image)
         return registered_image
+
+    def _translate_image(self, image):
+        return np.roll(image, (self.vertical_shift, self.horizontal_shift), axis=(0, 1))  # shift image
+
+    def _rotate_image(self, image):
+        return skimage.transform.rotate(image, self.main_channel_angle, cval=0)  # rotate image
+
+    def get_transformation_matrix(self):
+        if self.main_channel_angle is None:
+            raise ValueError("self.main_channel_angle must be set before calling self.get_transformation_matrix")
+        if self.vertical_shift is None:
+            raise ValueError("self.vertical_shift must be set before calling self.get_transformation_matrix")
+        if self.horizontal_shift is None:
+            raise ValueError("self.horizontal_shift must be set before calling self.get_transformation_matrix")
+
+        rotation_center = self.image.shape/2
+
+        return preprocessing.get_transformation_matrix(self.horizontal_shift, self.vertical_shift, self.main_channel_angle, rotation_center)
+
+        raise NotImplementedError
