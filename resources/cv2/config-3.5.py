28f04dd79fe5a9ca73d99609cc892c2e071c236f
Michael Mell <michael.mell@gmx.net>
2019-04-30 17:37:29 +0200

    Add self-built python-opencv modcule
    This was necessary, because for the pip-install of opencv-contrib-python, the Qt GUI does not work.
    See this links for more details:
    https://www.learnopencv.com/install-opencv-4-on-ubuntu-16-04/
    https://www.pyimagesearch.com/2018/08/15/how-to-install-opencv-4-on-ubuntu/
    Links about the issue:
    https://github.com/skvark/opencv-python/issues/46
    https://stackoverflow.com/questions/50187902/python-opencv-current-thread-not-objects-thread-on-imshow
    https://stackoverflow.com/questions/46449850/how-to-fix-the-error-qobjectmovetothread-in-opencv-in-python#

---
+++ resources/cv2/config-3.5.py
@@ -0,0 +1,3 @@
+PYTHON_EXTENSIONS_PATHS = [
+    os.path.join('/media/data_volume/install_pyopencv_4.1_from_source/second_try/OpenCV-master-py3/lib/python3.5/site-packages/cv2', 'python-3.5')
+] + PYTHON_EXTENSIONS_PATHS
---
+++ resources/cv2/config.py
@@ -0,0 +1,3 @@
+BINARIES_PATHS = [
+    os.path.join('/media/data_volume/install_pyopencv_4.1_from_source/second_try/installation/OpenCV-master', 'lib')
+] + BINARIES_PATHS
