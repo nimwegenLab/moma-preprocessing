Corrected the sign of the angle.

mmpreprocesspy/mmpreprocesspy/rotated_roi.py
@@ -33,7 +33,7 @@ class RotatedRoi(object):
         # aux.show_image(bounding_box_image)
         # cv.waitKey()
 
-        M = cv.getRotationMatrix2D(bounding_image_center, self.angle, 1.0)
+        M = cv.getRotationMatrix2D(bounding_image_center, -self.angle, 1.0)
         rotated_image = cv.warpAffine(bounding_box_image, M, (bounding_box_image.shape[1], bounding_box_image.shape[0]), cv.INTER_CUBIC)
 
         # cv.imshow("rotated_image", rotated_image)
@@ -51,14 +51,14 @@ class RotatedRoi(object):
         return cropped
 
     def rotate(self, rotation_center, rotation_angle):
-        self.angle = -rotation_angle   # Note: I am not sure, why I need to invert the sign for the angle, but it works this way
-        M = cv.getRotationMatrix2D((rotation_center[1],rotation_center[0]), self.angle, 1.0)
-        center_coordinate = np.array([self.center[1], self.center[0], 1])
+        M = cv.getRotationMatrix2D((rotation_center[0], rotation_center[1]), rotation_angle, 1.0)
+        center_coordinate = np.array([self.center[0], self.center[1], 1])
         new_center = M.dot(center_coordinate)
-        self.center = (new_center[1], new_center[0])
+        self.center = (new_center[0], new_center[1])
+        self.angle = rotation_angle   # Note: I am not sure, why I need to invert the sign for the angle, but it works this way
         pass
 
     @property
     def points(self):
-        rot_rect = (self.center, (self.width, self.height), self.angle)
+        rot_rect = (self.center, (self.width, self.height), -self.angle) #  NOTE-MM-2019-05-02: I do not understand why it has to be '-self.angle' instead of self.angle
         return np.int0(cv.boxPoints(rot_rect))