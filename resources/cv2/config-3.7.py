b2716eced4398b46c26a79880b8f1b6c7046b488
Michael Mell <michael.mell@unibas.ch>
2019-05-07 12:21:11 +0200

    Add files that were forgotten during commit 03ae3d13, where we updated the Python OpenCV version.

---
+++ resources/cv2/config-3.7.py
@@ -0,0 +1,3 @@
+PYTHON_EXTENSIONS_PATHS = [
+    os.path.join('/home/micha/Documents/opencv4_source_build/attempt_3/OpenCV-4_1-py3/lib/python3.7/site-packages/cv2', 'python-3.7')
+] + PYTHON_EXTENSIONS_PATHS
