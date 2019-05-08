d3288d8706a23bda50dc64c48762094b08f25688
Michael Mell <michael.mell@unibas.ch>
2019-05-08 12:19:26 +0200

    Add new integration test for dataset_08. Also move the MMStack for dataset_04 for orderliness.

--- mmpreprocesspy/test/test_preproc_fun.py
+++ mmpreprocesspy/test/test_preproc_fun.py
@@ -4,20 +4,30 @@ from unittest import TestCase
 
 
 class TestPreproc_fun(TestCase):
-    def test_preproc_fun(self):
+    def test__dataset_04(self):
         from mmpreprocesspy import preproc_fun
 
-        # '/home/micha/Documents/git/MM_Testing/Moma_stacks/20180531_gluIPTG5uM_lac_1_MMStack.ome.tif'
-
-        data_directory = '/home/micha/Documents/git/MM_Testing/MMStacks/20180531_gluIPTG5uM_lac_1_MMStack'
-        directory_to_save = '/home/micha/Documents/git/MM_Testing/MMStacks/20180531_gluIPTG5uM_lac_1_MMStack'
+        data_directory = '/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/MMStack'
+        directory_to_save = '/home/micha/Documents/git/MM_Testing/04_20180531_gluIPTG5uM_lac_1/MMStack'
         positions = [0]
         maxframe = 10
 
-        results_directory = directory_to_save + '/20180531_gluIPTG5uM_lac_1_MMStack/'
+        results_directory = directory_to_save + '/result/'
         if os.path.isdir(results_directory):
             shutil.rmtree(results_directory)
 
-        preproc_fun.preproc_fun(data_directory, directory_to_save, positions, maxframe)
+        preproc_fun.preproc_fun(data_directory, results_directory, positions, maxframe)
 
-        pass
+    def test__dataset_08(self):
+        from mmpreprocesspy import preproc_fun
+
+        data_directory = '/home/micha/Documents/git/MM_Testing/08_20190222_LB_SpentLB_TrisEDTA_LB_1/MMStack'
+        directory_to_save = '/home/micha/Documents/git/MM_Testing/08_20190222_LB_SpentLB_TrisEDTA_LB_1/MMStack'
+        positions = [0]
+        maxframe = 10
+
+        results_directory = directory_to_save + '/result/'
+        if os.path.isdir(results_directory):
+            shutil.rmtree(results_directory)
+
+        preproc_fun.preproc_fun(data_directory, results_directory, positions, maxframe)
