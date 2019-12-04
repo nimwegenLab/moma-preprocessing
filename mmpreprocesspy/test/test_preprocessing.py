e7082134d883090b4b71fd6d81bef39c18be72d0
Michael Mell <michael.mell@unibas.ch>
2019-12-04 11:22:45 +0100

    Add tests with additional datasets.

--- mmpreprocesspy/test/test_preprocessing.py
+++ mmpreprocesspy/test/test_preprocessing.py
@@ -1,14 +1,38 @@
 from unittest import TestCase
-import skimage.transform
-from skimage.io import imread
+
 import matplotlib.pyplot as plt
 import mmpreprocesspy.dev_auxiliary_functions as dev_aux
+import skimage.transform
+from skimage.io import imread
+from skimage.transform import AffineTransform, warp
+import numpy as np
+import cv2
 
 
 class TestPreprocessing(TestCase):
     test_data_base_path = '/home/micha/Documents/01_work/git/MM_Testing'
 
-    def test__find_channels_in_region_new(self):
+    def test__find_channels_in_region_dataset_10(self):
+        from mmpreprocesspy import preprocessing
+
+        image = imread("./resources/10_20190424_hi2_hi3_med2_rplN_4_MMStack.ome-2.tif")
+        centers = preprocessing.find_channels_in_region_new(image)
+        image_with_channel_indicators = get_image_with_lines(image, centers)
+
+        plt.imshow(image_with_channel_indicators, cmap="gray")
+        plt.show()
+
+    def test__find_channels_in_region_dataset_4(self):
+        from mmpreprocesspy import preprocessing
+
+        image = imread("./resources/04_20180531_gluIPTG5uM_lac_1_MMStack.ome-2_channel_region.tif")
+        centers = preprocessing.find_channels_in_region_new(image)
+        image_with_channel_indicators = get_image_with_lines(image, centers)
+
+        plt.imshow(image_with_channel_indicators, cmap="gray")
+        plt.show()
+
+    def test__find_channels_in_region_dataset_11(self):
         from mmpreprocesspy import preprocessing
 
         image = imread("./resources/rotated_channel_region.tiff")
@@ -92,8 +116,25 @@ def read_tiff_to_nparray(image_path):
     #     im2.show()
 
 def get_image_with_lines(channel_region_image, channel_positions):
-    new_image = channel_region_image.copy()
+    new_image = np.float32(channel_region_image).copy()
+    new_image /= np.max(new_image)
     for pos in channel_positions:
-        new_image[int(pos):int(pos)+4, :] = 2
+        new_image[int(pos), :] = 1
     return new_image
 
+def shift(image, vector):
+    transform = AffineTransform(translation=vector)
+    output_shape = (image.shape[0] - vector[1], image.shape[1] - vector[0])
+    shifted = warp(image, transform, mode='constant', preserve_range=True, output_shape=output_shape)
+    shifted_image = shifted.astype(image.dtype)
+    return shifted_image[:]
+
+
+
+    # M = np.float32([[1, 0, vector[0]], [0, 1, vector[1]]])
+    # # Read image from disk.
+    # (rows, cols) = image.shape[:2]
+    #
+    # # warpAffine does appropriate shifting given the
+    # # translation matrix.
+    # return cv2.warpAffine(image, M, (cols, rows))
