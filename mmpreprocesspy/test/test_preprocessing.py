dfe5b9dcd4dcd4820b1cb2868a7c53372a78facd
Michael Mell <michael.mell@unibas.ch>
2021-01-11 17:15:18 +0100

    No longer use the fourier-space spectrum/periodicity to determine locations of the GLs. Instead we now scan for plateaus (regions of constant values) in the maxima value-projection along the main-channel. This is more robust and more straight-forward in case we need to debug. I do not see a draw-back with this method. Tests have been updated and all pass.

--- mmpreprocesspy/test/test_preprocessing.py
+++ mmpreprocesspy/test/test_preprocessing.py
@@ -17,78 +17,78 @@ class TestPreprocessing(TestCase):
         tests.append({'name': 'dataset_16',
                       'path': "./resources/test_preprocessing_data/16_thomas__20201229_glc_lac_1_MMStack__Pos0__rotated.tif",
                       'angle': 0,
-                      'glt': 100,
-                      'centers': [0, 0]})
-        # tests.append({'name': 'dataset_15',
-        #               'path': "./resources/test_preprocessing_data/15_lis__20201119_VNG1040_AB2h_2h_1_MMStack.ome.tif",
-        #               'angle': 90,
-        #               'glt': 100,
-        #               'centers': [556, 1409]})
-        # tests.append({'name': 'dataset_14',
-        #               'path': "./resources/test_preprocessing_data/14_thomas_20201228_glc_ara_1__Pos0__rotated.tif",
-        #               'angle': -.5,
-        #               'glt': 300,
-        #               'centers': [0, 0]})
-        # tests.append({'name': 'dataset_13',
-        #               'path': "./resources/test_preprocessing_data/13_20200128_glcIPTG_glc_1__MMStack.ome.tif",
-        #               'angle': 0,
-        #               'glt': 100,
-        #               'centers': [477, 752]})
-        # tests.append({'name': 'dataset_12',
-        #               'path': "./resources/test_preprocessing_data/12_20190816_Theo_MMStack.ome.tif",
-        #               'angle': 0,
-        #               'glt': 100,
-        #               'centers': [526]})
-        # tests.append({'name': 'dataset_11',
-        #               'path': "./resources/test_preprocessing_data/11_20190910_glc_spcm_1_MMStack.ome.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [555, 1391]})
-        # tests.append({'name': 'dataset_10',
-        #               'path': "./resources/test_preprocessing_data/10_20190424_hi2_hi3_med2_rplN_glu_gly.ome.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [392, 1249]})
-        # tests.append({'name': 'dataset_9',
-        #               'path': "./resources/test_preprocessing_data/09_20190325_hi1_hi2_med1_rpmB_glu_gly_pl_chr_1.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [519, 1372]})
-        # tests.append({'name': 'dataset_8',
-        #               'path': "./resources/test_preprocessing_data/08_20190222_LB_SpentLB_TrisEDTA_LB_1.tif",
-        #               'angle': -4,
-        #               'glt': 300,
-        #               'centers': [591, 1441]})
-        # tests.append({'name': 'dataset_7',
-        #               'path': "./resources/test_preprocessing_data/07_20181203_glu_lac_switch16h_1__Pos0.tif",
-        #               'angle': 0,
-        #               'glt': 100,
-        #               'centers': [478]})
-        # tests.append({'name': 'dataset_6',
-        #               'path': "./resources/test_preprocessing_data/06_20180313_glu_lac_switch24h_1__Pos0.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [528]})
-        # tests.append({'name': 'dataset_5',
-        #               'path': "./resources/test_preprocessing_data/05_20180404_glu_lacCM-ara_1__Pos0.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [525]})
-        # tests.append({'name': 'dataset_4',
-        #               'path': "./resources/test_preprocessing_data/04_20180531_gluIPTG5uM_lac_1__Pos0.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [513]})
-        # tests.append({'name': 'dataset_3',
-        #               'path': "./resources/test_preprocessing_data/03_20180604_gluIPTG10uM_lac_lacIoe_1__Pos0.tif",
-        #               'angle': 0,
-        #               'glt': 300,
-        #               'centers': [496]})
-        # tests.append({'name': 'dataset_0',
-        #               'path': "./resources/test_preprocessing_data/00_20150710_mmtest_2ch__Pos0__rotated.tif",
-        #               'angle': -1.5,
-        #               'glt': 300,
-        #               'centers': [678]})
+                      'glt': 200,
+                      'centers': [538, 1343]})
+        tests.append({'name': 'dataset_15',
+                      'path': "./resources/test_preprocessing_data/15_lis__20201119_VNG1040_AB2h_2h_1_MMStack.ome.tif",
+                      'angle': 90,
+                      'glt': 200,
+                      'centers': [573, 1409]})
+        tests.append({'name': 'dataset_14',
+                      'path': "./resources/test_preprocessing_data/14_thomas_20201228_glc_ara_1__Pos0__rotated.tif",
+                      'angle': -.5,
+                      'glt': 300,
+                      'centers': [489, 1315]})
+        tests.append({'name': 'dataset_13',
+                      'path': "./resources/test_preprocessing_data/13_20200128_glcIPTG_glc_1__MMStack.ome.tif",
+                      'angle': 0,
+                      'glt': 200,
+                      'centers': [465, 752]})
+        tests.append({'name': 'dataset_12',
+                      'path': "./resources/test_preprocessing_data/12_20190816_Theo_MMStack.ome.tif",
+                      'angle': 0,
+                      'glt': 200,
+                      'centers': [526]})
+        tests.append({'name': 'dataset_11',
+                      'path': "./resources/test_preprocessing_data/11_20190910_glc_spcm_1_MMStack.ome.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [518, 1355]})
+        tests.append({'name': 'dataset_10',
+                      'path': "./resources/test_preprocessing_data/10_20190424_hi2_hi3_med2_rplN_glu_gly.ome.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [392, 1249]})
+        tests.append({'name': 'dataset_9',
+                      'path': "./resources/test_preprocessing_data/09_20190325_hi1_hi2_med1_rpmB_glu_gly_pl_chr_1.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [519, 1372]})
+        tests.append({'name': 'dataset_8',
+                      'path': "./resources/test_preprocessing_data/08_20190222_LB_SpentLB_TrisEDTA_LB_1.tif",
+                      'angle': -4,
+                      'glt': 300,
+                      'centers': [591, 1441]})
+        tests.append({'name': 'dataset_7',
+                      'path': "./resources/test_preprocessing_data/07_20181203_glu_lac_switch16h_1__Pos0.tif",
+                      'angle': 0,
+                      'glt': 200,
+                      'centers': [461]})
+        tests.append({'name': 'dataset_6',
+                      'path': "./resources/test_preprocessing_data/06_20180313_glu_lac_switch24h_1__Pos0.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [512]})
+        tests.append({'name': 'dataset_5',
+                      'path': "./resources/test_preprocessing_data/05_20180404_glu_lacCM-ara_1__Pos0.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [513]})
+        tests.append({'name': 'dataset_4',
+                      'path': "./resources/test_preprocessing_data/04_20180531_gluIPTG5uM_lac_1__Pos0.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [513]})
+        tests.append({'name': 'dataset_3',
+                      'path': "./resources/test_preprocessing_data/03_20180604_gluIPTG10uM_lac_lacIoe_1__Pos0.tif",
+                      'angle': 0,
+                      'glt': 300,
+                      'centers': [484]})
+        tests.append({'name': 'dataset_0',
+                      'path': "./resources/test_preprocessing_data/00_20150710_mmtest_2ch__Pos0__rotated.tif",
+                      'angle': -1.5,
+                      'glt': 300,
+                      'centers': [660]})
         return tests
 
     def test__find_channel_regions(self):
@@ -112,13 +112,13 @@ class TestPreprocessing(TestCase):
                 imdata = skimage.transform.rotate(imdata, rotation_angle)
                 region_list = preprocessing.find_channel_regions(imdata, use_smoothing=True, minimum_required_growthlane_length=growthlane_length_threshold)
 
-                if test['name'] == 'dataset_14' or test['name'] == 'dataset_16':
-                    plt.imshow(imdata, cmap='gray')
-                    for ind, region in enumerate(region_list):
-                        plt.axvline(region.start, color='r')
-                        plt.axvline(region.end, color='g')
-                    plt.title(test['name'])
-                    plt.show()
+                # if test['name'] == 'dataset_14' or test['name'] == 'dataset_16':
+                #     plt.imshow(imdata, cmap='gray')
+                #     for ind, region in enumerate(region_list):
+                #         plt.axvline(region.start, color='r')
+                #         plt.axvline(region.end, color='g')
+                #     plt.title(test['name'])
+                #     plt.show()
 
                 for ind, region in enumerate(region_list):
                     expected = region_centers[ind]
