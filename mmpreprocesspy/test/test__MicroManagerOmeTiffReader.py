e8b4875b40362064f6e6252b99a83290320d1d63
Michael Mell <michael.mell@unibas.ch>
2021-01-18 14:57:44 +0100

    Add test for reading different frame indexes from datasets and makes sure they are different (which is currently not the case for datasets 15 and 17).

--- mmpreprocesspy/test/test__MicroManagerOmeTiffReader.py
+++ mmpreprocesspy/test/test__MicroManagerOmeTiffReader.py
@@ -21,26 +21,97 @@ class test_MicroManagerOmeTiffReader(TestCase):
         self.assertTrue(np.all(expected == image_stack), msg="result image does not match expected image")
 
 
-    def test__get_image_stack__returns_different_images_for_different_frame_indexes(self):
+    def get_test_data___test__get_image_stack__returns_different_images_for_different_frame_indexes(self):
+        test_data_base_path = '/home/micha/Documents/01_work/git/MM_Testing'
+        test_configs = []
+
+        test_configs.append({'name': 'dataset_17',
+                             'path': os.path.join(test_data_base_path, '17_lis_20201218_VNG40_AB6min_2h_1_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_16',
+                             'path': os.path.join(test_data_base_path, '16_thomas_20201229_glc_lac_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_15',
+                             'path': os.path.join(test_data_base_path, '15_lis_20201119_VNG1040_AB2h_2h_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_14',
+                             'path': os.path.join(test_data_base_path, '14_thomas_20201228_glc_ara_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_13',
+                             'path': os.path.join(test_data_base_path, '13_20200128_glcIPTG_glc_1/MMStack/RawData/measurement/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_12',
+                             'path': os.path.join(test_data_base_path, '12_20190816_Theo/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_11',
+                             'path': os.path.join(test_data_base_path, '11_20190910_glc_spcm_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_10',
+                             'path': os.path.join(test_data_base_path, '10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/measurement/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_8',
+                             'path': os.path.join(test_data_base_path, '08_20190222_LB_SpentLB_TrisEDTA_LB_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+        test_configs.append({'name': 'dataset_4',
+                             'path': os.path.join(test_data_base_path, '04_20180531_gluIPTG5uM_lac_1/MMStack/'),
+                             'position_index': 0,
+                             'min_frame_index': 0,
+                             'max_frame_index': 8})
+
+        return test_configs
+
+    def test__get_image_stack__returns_different_images_for_different_frame_indexes__17_lis_20201218_VNG40_AB6min_2h_1_1(self):
         from mmpreprocesspy.MicroManagerOmeTiffReader import MicroManagerOmeTiffReader
 
-        test_data_base_path = '/home/micha/Documents/01_work/git/MM_Testing'
-        path = os.path.join(test_data_base_path, '16_thomas_20201229_glc_lac_1/MMStack/')
-        dataset = MicroManagerOmeTiffReader(path)
+        test_configs = self.get_test_data___test__get_image_stack__returns_different_images_for_different_frame_indexes()
 
-        position_index = 0
-        min_frame = 00
-        max_frame = 19
+        for test_config in test_configs:
+            with self.subTest(test=test_config['name']):
+                position_index = test_config['position_index']
+                min_frame = test_config['min_frame_index']
+                max_frame = test_config['max_frame_index']
+                path = test_config['path']
 
-        for frame_index in range(min_frame, max_frame):
-            current_frame = dataset.get_image_stack(frame_index=frame_index,
-                                                        position_index=position_index)
-            next_frame = dataset.get_image_stack(frame_index=frame_index + 1,
-                                                        position_index=position_index)
+                dataset = MicroManagerOmeTiffReader(path)
 
-            print('')
-            print(f'frame {frame_index}:')
-            print(f'PhC equal: {np.all(current_frame[:, :, 0] == next_frame[:, :, 0])}')
-            print(f'FL equal: {np.all(current_frame[:, :, 1] == next_frame[:, :, 1])}')
+                for frame_index in range(min_frame, max_frame):
+                    current_frame = dataset.get_image_stack(frame_index=frame_index,
+                                                                position_index=position_index)
+                    next_frame = dataset.get_image_stack(frame_index=frame_index + 1,
+                                                                position_index=position_index)
 
-        # self.assertTrue(np.all(expected == image_stack), msg="result image does not match expected image")
+                    # print('')
+                    # print(f'frame {frame_index}:')
+                    # print(f'PhC equal: {np.all(current_frame[:, :, 0] == next_frame[:, :, 0])}')
+                    # print(f'FL equal: {np.all(current_frame[:, :, 1] == next_frame[:, :, 1])}')
+
+                    self.assertFalse(np.all(current_frame[:, :, 0] == next_frame[:, :, 0]),
+                                            msg=f'consecutive PhC frames are equal: frame_index {frame_index}')
+                    self.assertFalse(np.all(current_frame[:, :, 1] == next_frame[:, :, 1]),
+                                            msg=f'consecutive FL frames are equal: frame_index {frame_index}')
+
+                    # if np.all(current_frame[:, :, 0] == next_frame[:, :, 0]):
+                    #     import matplotlib.pyplot as plt
+                    #     plt.imshow(current_frame[:, :, 0])
+                    #     plt.show()
+                    #     pass
+
+                # self.assertTrue(np.all(expected == image_stack), msg="result image does not match expected image")
