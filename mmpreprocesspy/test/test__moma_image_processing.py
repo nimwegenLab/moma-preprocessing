847362e3814b8b52a35c242ec4f05a9a0e225919
Michael Mell <michael.mell@unibas.ch>
2021-02-04 11:38:20 +0100

    Add method to test, if the image shift is correctly recovered.

--- mmpreprocesspy/test/test__moma_image_processing.py
+++ mmpreprocesspy/test/test__moma_image_processing.py
@@ -5,27 +5,37 @@ from skimage.transform import AffineTransform, warp
 class test_MomaImageProcessor(TestCase):
     data_dir = os.path.join(os.path.dirname(__file__), 'resources/data__test__moma_image_processing')
 
-    def test__bla(self):
+    def test__image_shift_is_correctly_detected(self):
+        import numpy as np
         import tifffile as tff
         import matplotlib.pyplot as plt
         from mmpreprocesspy.moma_image_processing import MomaImageProcessor
 
+        original_image = tff.imread(os.path.join(self.data_dir, '12_20190816_Theo_MMStack.ome.tif'))
         sut = MomaImageProcessor()
-        original_image = tff.imread(os.path.join(data_dir, '12_20190816_Theo_MMStack.ome.tif'))
         sut._image_for_registration = original_image
-        shifted_image = self.support__shift_image(original_image, [50, 50])
 
-        fig, ax = plt.subplots(1, 2, figsize=(5, 10))
-        ax[0].imshow(original_image)
-        ax[1].imshow(shifted_image)
-        plt.show()
+        shifts_x_y = [5.7, 67.3]
 
-        sut.determine_image_shift(self, image)
-        pass
+        for xshift in shifts_x_y:
+            for yshift in shifts_x_y:
+                expected_shift = [xshift, yshift]
 
-    def support__shift_image(image, vector):
+                shifted_image = self.support__shift_image(original_image, expected_shift)
+
+                sut.determine_image_shift(shifted_image)
+                actual_shift = (sut.horizontal_shift, sut.vertical_shift)
+
+                np.testing.assert_array_almost_equal(expected_shift,
+                                                     actual_shift,
+                                                     decimal=2,
+                                                     err_msg=f'expected_shift was not recovered: {expected_shift}\n'
+                                                     f'actual shift: {actual_shift}')
+
+    def support__shift_image(self, image, vector):
         transform = AffineTransform(translation=vector)
         shifted = warp(image, transform, mode='wrap', preserve_range=True)
         shifted = shifted.astype(image.dtype)
+        return shifted
 
 
