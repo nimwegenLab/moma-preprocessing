46f85377f8d2b96c09c1d9fce7fd0f3b0e612a40
Michael Mell <michael.mell@unibas.ch>
2022-04-04 11:20:03 +0200

    Remove unused imports.

--- exploration/test_preproc_fun.py
+++ exploration/test_preproc_fun.py
@@ -11,7 +11,6 @@ class TestPreproc_fun(TestCase):
         self.test_data_base_path = os.getenv('PREPROCDATADIR')
 
     def test__35__lis__20220320__repeat(self):
-        from mmpreprocesspy import preproc_fun
         data_directory = self.test_data_base_path + '/35__lis__20220320__repeat/MMSTACK/20220320_VNG1040_AB2h_1_Frame0-478_resaved/'
         directory_to_save = self.test_data_base_path + '/35__lis__20220320__repeat/result_without_flatfield/'
         gl_detection_template_path = self.test_data_base_path + '/35__lis__20220320__repeat/TEMPLATE_MICHAEL_v002/template_config.json'
@@ -48,7 +47,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__34__nevil__20220302(self):
-        from mmpreprocesspy import preproc_fun
         data_directory = self.test_data_base_path + '/34__nevil__20220302/MMStack/202200302_glc_spcm_stress_1/'
         directory_to_save = self.test_data_base_path + '/34__nevil__20220302/result_without_flatfield/'
         gl_detection_template_path = self.test_data_base_path + '/34__nevil__20220302/TEMPLATE/template_config.json'
@@ -85,7 +83,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__33__lis__20220320(self):
-        from mmpreprocesspy import preproc_fun
         data_directory = self.test_data_base_path + '/33__lis__20220320/MMStack/20220320_VNG1040_AB2h_1/'
         directory_to_save = self.test_data_base_path + '/33__lis__20220320/result_without_flatfield/'
         gl_detection_template_path = self.test_data_base_path + '/33__lis__20220320/TEMPLATE/template_config.json'
@@ -122,7 +119,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__32__lis__20220128(self):
-        from mmpreprocesspy import preproc_fun
         data_directory = self.test_data_base_path + '/32__lis__20220128/MMStack/'
         directory_to_save = self.test_data_base_path + '/32__lis__20220128/result_without_flatfield/'
         gl_detection_template_path = self.test_data_base_path + '/32__lis__20220128/TEMPLATE/template_config.json'
@@ -159,7 +155,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__31__dany__20190515(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/31__dany__20190515/MMStack/'
         directory_to_save = self.test_data_base_path + '/31__dany__20190515/result_without_flatfield/'
@@ -196,7 +191,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__30__lis_20210813__ignore_frames(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/30__lis_20210813/MMStack/'
         directory_to_save = self.test_data_base_path + '/30__lis_20210813/result_without_flatfield/'
@@ -233,7 +227,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__30__lis_20210813(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/30__lis_20210813/MMStack/'
         directory_to_save = self.test_data_base_path + '/30__lis_20210813/result_without_flatfield/'
@@ -268,7 +261,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_29_without_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/29__dany__20210723_aceglyaa_pl/MMStack/'
         directory_to_save = self.test_data_base_path + '/29__dany__20210723_aceglyaa_pl/result_without_flatfield/'
@@ -302,7 +294,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_28_without_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/28__theo__20210506/MMStack/'
         directory_to_save = self.test_data_base_path + '/28__theo__20210506/result_without_flatfield/'
@@ -336,7 +327,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_26__Lis__20210304_defocus_stack_z_split(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/26__Lis__20210304_defocus_stack/MMStack_z_plane_split/'
         directory_to_save = self.test_data_base_path + '/26__Lis__20210304_defocus_stack/output/'
@@ -368,7 +358,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_26__Lis__20210304_defocus_stack(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/26__Lis__20210304_defocus_stack/MMStack/'
         directory_to_save = self.test_data_base_path + '/26__Lis__20210304_defocus_stack/output/'
@@ -401,7 +390,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_25_without_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/25__dany__20210423/MMStack/'
         directory_to_save = self.test_data_base_path + '/25__dany__20210423/result_with_flatfield/'
@@ -432,7 +420,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_24_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/24__lis__20210303/MMStack/'
         directory_to_save = self.test_data_base_path + '/24__lis__20210303/result_with_flatfield/'
@@ -459,7 +446,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_23_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/23__thomas__20170403_synthPromsHi_glc_aMG_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/23__thomas__20170403_synthPromsHi_glc_aMG_1/MMStack/result_no_flatfield/'
@@ -484,7 +470,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_21_no_flatfield')
 
     def test__dataset_20_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         path = os.path.join(self.test_data_base_path, '20__theo__20210122')
         data_directory = os.path.join(path, '20210122_glu_lac_1')
@@ -511,7 +496,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_20_with_flatfield')
 
     def test__dataset_19_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/19__dany__20201123_comlac_3conds_5/MMStack/'
         directory_to_save = self.test_data_base_path + '/19__dany__20201123_comlac_3conds_5/MMStack/result_no_flatfield/'
@@ -533,7 +517,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_19_no_flatfield')
 
     def test__dataset_17_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/17_lis_20201218_VNG40_AB6min_2h_1_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/17_lis_20201218_VNG40_AB6min_2h_1_1/MMStack/result_no_flatfield/'
@@ -556,7 +539,6 @@ class TestPreproc_fun(TestCase):
 
 
     def test__dataset_16_no_flatfield_using_gl_detection_template(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/16_thomas_20201229_glc_lac_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/16_thomas_20201229_glc_lac_1/MMStack/result_no_flatfield_with_gl_detection_template/'
@@ -580,7 +562,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_16_no_flatfield_with_gl_detection_template')
 
     def test__dataset_16_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/16_thomas_20201229_glc_lac_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/16_thomas_20201229_glc_lac_1/MMStack/result_no_flatfield/'
@@ -600,7 +581,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_16_no_flatfield')
 
     def test__dataset_15_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/15_lis_20201119_VNG1040_AB2h_2h_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/15_lis_20201119_VNG1040_AB2h_2h_1/MMStack/result_no_flatfield/'
@@ -630,7 +610,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_15_no_flatfield')
 
     def test__dataset_14_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/14_thomas_20201228_glc_ara_1/MMStack/'
         directory_to_save = self.test_data_base_path + '/14_thomas_20201228_glc_ara_1/MMStack/result_no_flatfield/'
@@ -651,7 +630,6 @@ class TestPreproc_fun(TestCase):
 
 
     def test__dataset_13_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/13_20200128_glcIPTG_glc_1/MMStack/RawData/measurement'
         flatfield_directory = self.test_data_base_path + '/13_20200128_glcIPTG_glc_1/MMStack/RawData/flatfield'
@@ -676,7 +654,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_13_with_flatfield')
 
     def test__dataset_10_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/measurement'
         flatfield_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/flatfield'
@@ -698,7 +675,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_10_with_flatfield')
 
     def test__dataset_11_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/11_20190910_glc_spcm_1/MMStack'
         flatfield_directory = self.test_data_base_path + '/11_20190910_glc_spcm_1/MMStack/flatfield'
@@ -720,7 +696,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_11_with_flatfield')
 
     def test__dataset_11_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/11_20190910_glc_spcm_1/MMStack'
         directory_to_save = self.test_data_base_path + '/11_20190910_glc_spcm_1/MMStack/result_no_flatfield/'
@@ -740,7 +715,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_11_no_flatfield')
 
     def test__dataset_12_with_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/12_20190816_Theo/MMStack/'
         flatfield_directory = self.test_data_base_path + '/12_20190816_Theo/MMStack/flatfield'
@@ -763,7 +737,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_12_with_flatfield')
 
     def test__dataset_12_no_flatfield_using_gl_detection_template(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/12_20190816_Theo/MMStack/'
         directory_to_save = self.test_data_base_path + '/12_20190816_Theo/MMStack/result_no_flatfield_using_gl_detection_template/'
@@ -788,7 +761,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_12_no_flatfield_using_gl_detection_template')
 
     def test__dataset_12_no_flatfield(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/12_20190816_Theo/MMStack/'
         directory_to_save = self.test_data_base_path + '/12_20190816_Theo/MMStack/result_no_flatfield/'
@@ -809,7 +781,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_12_no_flatfield')
 
     def test__dataset_11(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/11_20190910_glc_spcm_1/MMStack'
         directory_to_save = data_directory
@@ -826,7 +797,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_11')
 
     def test__dataset_04(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/04_20180531_gluIPTG5uM_lac_1/MMStack'
         directory_to_save = data_directory
@@ -845,7 +815,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_04')
 
     def test__dataset_08(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/08_20190222_LB_SpentLB_TrisEDTA_LB_1/MMStack'
         directory_to_save = data_directory
@@ -864,7 +833,6 @@ class TestPreproc_fun(TestCase):
                                           title='test__dataset_08')
 
     def test__dataset_10(self):
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/measurement'
         flatfield_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/flatfield'
@@ -892,7 +860,6 @@ class TestPreproc_fun(TestCase):
         # There is jump of the image position in dataset 10 going from frame 603 to 604. This trips up the current
         # current implementation. This test was created for debugging this issue.
 
-        from mmpreprocesspy import preproc_fun
 
         data_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/measurement'
         flatfield_directory = self.test_data_base_path + '/10_20190424_hi2_hi3_med2_rplN_glu_gly/MMStack/RawData/flatfield'
@@ -915,7 +882,6 @@ class TestPreproc_fun(TestCase):
                                 growthlane_length_threshold=growthlane_length_threshold)
 
     def test__get_gl_tiff_path(self):
-        from mmpreprocesspy import preproc_fun
 
         gl_file_path = preproc_fun.get_gl_tiff_path('/path/to/file', 'experiment_name', '1', '5')
         print(gl_file_path)
