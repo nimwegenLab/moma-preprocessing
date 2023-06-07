c0941359a37d74be22dcc6e73a4bc2cf5944799e
Michael Mell <michael.mell@unibas.ch>
2023-06-07 12:24:43 +0200

    Extend build_docker_image.sh so that it avoids dangling images.

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-docker build .. -t mmpreprocesspy:v0.2.0
+###
+# This script builds the docker image to containerize the mmpreprocesspy package.
+###
+
+
+CONTAINER_TAG="mmpreprocesspy:v0.2.0"
+
+# Get previously existing image with same tag to remove it at end of script and avoid dangling images.
+PREV_IMAGE=$(docker images --filter=reference="${CONTAINER_TAG}" -q)
+
+docker build .. -t ${CONTAINER_TAG}
+
+# Remove previous image with the same tag.
+if [[ -z "${PREV_IMAGE}" ]]; then
+  docker rmi "${PREV_IMAGE}"
+fi
