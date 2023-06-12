0c853ffd09b00d0a65fa8d6f9de9017d5d7f98a6
Michael Mell <michael.mell@unibas.ch>
2023-06-12 14:48:48 +0200

    Refactor code to use helpers.sh

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -6,12 +6,12 @@
 
 source helpers.sh
 
-CONTAINER_TAG=$(get_image_tag)
+PREV_IMAGE_TAG=$(get_image_tag)
 
 # Get previously existing image with same tag to remove it at end of script and avoid dangling images.
-PREV_IMAGE=$(docker images --filter=reference="${CONTAINER_TAG}" -q)
+PREV_IMAGE=$(docker images --filter=reference="${PREV_IMAGE_TAG}" -q)
 
-docker build .. -t ${CONTAINER_TAG}
+docker build .. -t "${PREV_IMAGE_TAG}"
 
 # Remove previous image with the same tag.
 if [[ -z "${PREV_IMAGE}" ]]; then
