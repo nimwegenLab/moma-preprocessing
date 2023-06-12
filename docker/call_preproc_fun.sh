9c4efd8465a95e7205f3634fafb81e14f247d738
Michael Mell <michael.mell@unibas.ch>
2023-06-12 14:54:36 +0200

    Clean up code.

--- docker/call_preproc_fun.sh
+++ docker/call_preproc_fun.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Hello world!"
-echo "These were the arguments: " "$@"
-
-#conda activate mmpreprocesspy_conda_environment
 source activate mmpreprocesspy_conda_environment
-
-#python /preprocessing/call_preproc_fun.py -i "/data/input_path" -o "/data/output_path" "$@"
-
-echo
-
-echo "/preprocessing/call_preproc_fun.sh" "$@"
-
-echo
-
-ARGS="$@"
-
-echo "ARGS: ${ARGS}"
-
-echo
-
 eval "python /preprocessing/call_preproc_fun.py" "$@"
-
-echo
-
 conda deactivate
